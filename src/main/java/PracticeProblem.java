b32adc5690f9162915b0831ef53af5bd1d94cb78
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-18 18:51:53 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,54 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
+
+	public static int find(int[] array, int number) {
+		
+		int firstOcc = -1;
+
+		for (int i=0; i<array.length; i++) {
+			if (array[i] == number) {
+				firstOcc = i;
+				return firstOcc;
+			}
+		}
+
+		return firstOcc;
+
+	}
+	
+	public static int findLast(String[] array, String string) {
+
+		int lastOcc = -1;
+
+		for (int i=0; i<array.length; i++) {
+			if (array[i] == string) {
+				lastOcc = i;
+			}
+		}
+
+		return lastOcc;
 
 	}
 
-	
+	public static int findSecond(char[] array, char character) {
+
+		int secOcc = -1;
+		int occ = 0;
+
+		for (int i=0; i<array.length; i++) {
+			if (array[i]==character) {
+				secOcc = i;
+				occ++;
+				if (occ>=2) {
+					secOcc = i;
+					return secOcc;
+				}
+
+			}
+		}
+
+		return secOcc;
+	}
 
 }
